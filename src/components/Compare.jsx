94dc269cba2995c724094234ca46360ffe9fb5d4
researchersec <109282836+researchersec@users.noreply.github.com>
2024-08-30 03:05:42 +0200

    Create Compare.jsx

---
+++ src/components/Compare.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Compare() {
+  return (
+    <div>
+      <h2>Compare Cars</h2>
+      {/* Implement comparison logic here */}
+    </div>
+  );
+}
+
+export default Compare;
